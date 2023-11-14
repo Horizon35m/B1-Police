fd72624c13f5e52a672b948d4536f82b66fbda79
HRZN <Horizon35m@users.noreply.github.com>
2023-11-14 15:13:40 -0500

    dawd

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'B1 Studios - HRZN'
+description 'Advanced Police Job for ESX'
+version '0.0.1'
+
+lua54 "yes"
+
+client_scripts {
+    'function_cl.lua',
+    'menu.lua',
+    'interaction.lua'
+}
+
+server_scripts {
+    'function_sv.lua',
+}
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua',
+}
